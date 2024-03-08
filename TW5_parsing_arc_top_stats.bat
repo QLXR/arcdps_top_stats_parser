2280084d957e5896316a57fd1e8a616b6376eae3
QLXR <hqsrt2006@hotmail.com>
2024-03-08 17:21:01 -0500

    change_bat

--- TW5_parsing_arc_top_stats.bat
+++ TW5_parsing_arc_top_stats.bat
@@ -1,2 +1,6 @@
+robocopy /mir %4 %1
+mkdir empty_folder
+robocopy /mir empty_folder %4
+rmdir empty_folder
 for %%i in (%1\*.zevtc) do (%2\GuildWars2EliteInsights.exe -c %3\EI_config\EI_detailed_json_combat_replay.conf "%%i")
 python %3/TW5_parse_top_stats_detailed.py %1
