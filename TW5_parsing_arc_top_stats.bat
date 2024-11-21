b09bde7b337b4be531f7a4b4e632108ea22d8009
QLXR <QLXR@users.noreply.github.com>
2024-11-21 18:11:09 -0500

    Change to  CLI

--- TW5_parsing_arc_top_stats.bat
+++ TW5_parsing_arc_top_stats.bat
@@ -2,5 +2,5 @@ robocopy /mir %3 %1
 mkdir empty_folder
 robocopy /mir empty_folder %3
 rmdir empty_folder
-for %%i in (%1\*.zevtc) do (%2\GuildWars2EliteInsights.exe -c %~dp0\EI_config\EI_detailed_json_combat_replay.conf "%%i")
+for %%i in (%1\*.zevtc) do (%2\GuildWars2EliteInsights-CLI.exe -c %~dp0\EI_config\EI_detailed_json_combat_replay.conf "%%i")
 python %~dp0/TW5_parse_top_stats_detailed.py %1
